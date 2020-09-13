7b1b140ece42641bdd9a2fc2aa9eecf9e0dacd39
AyushiAgarwal2399 <71220991+AyushiAgarwal2399@users.noreply.github.com>
2020-09-14 00:09:17 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,19 +1,21 @@
 <?php 
     $link = mysqli_connect("localhost", "root", "", "assignment");
-    if(!$link){
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-    }
+    $con = mysqli_connect("localhost","root","","assignment") or die("notconnected");
+    
+    if(isset($_POST['submit']))
+    {
     $fname = $_POST["fname"];
     $lname = $_POST["lname"];
     $pass = $_POST["pass"];
     $username = $_POST["username"];
     $sql = "INSERT INTO loginsystem VALUES ('$fname', '$lname', '$pass', '$username')";
-    if(mysqli_query($link, $sql)){
-        echo "New Record created successfully";
-        header('Location: login.html');
+    if(mysqli_query($con,$sql))
+    {
+	echo "<h3>Data is inserted succesfully </h3>";
+	header('Location: login.html');
     }
     else{
         echo "Error".mysqli.error($link);
     }
-
+ 
 ?>
